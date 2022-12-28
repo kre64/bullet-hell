0b55febf8ee685c45909247b7e6710c80c5ecbd7
kre64 <krecoding@gmail.com>
2022-12-28 13:23:03 -0500

    enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+  public GameObject slimePrefab;
+  public GameObject bigSlimePrefab;
+  public float slimeInterval = 1f;
+  public float bigSlimeInterval = 4f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    StartCoroutine(spawnEnemy(slimeInterval, slimePrefab));
+    StartCoroutine(spawnEnemy(bigSlimeInterval, bigSlimePrefab));
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private IEnumerator spawnEnemy(float interval, GameObject enemy)
+  {
+    yield return new WaitForSeconds(interval);
+    GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+    StartCoroutine(spawnEnemy(interval, enemy));
+  }
+}
