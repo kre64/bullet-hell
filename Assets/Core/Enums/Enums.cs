5cdf09f73a60bd0273abfb01d090c6a1db1c7a8d
kre64 <krecoding@gmail.com>
2023-01-04 22:33:41 -0500

    Add enums for game state/scenes

---
+++ Assets/Core/Enums/Enums.cs
@@ -0,0 +1,8 @@
+
+public enum WeaponType { Pistol, Shotgun, Lasergun }
+
+public enum HeartStatus { Empty = 0, Half = 1, Full = 2 }
+
+public enum GameState { MainMenu, InGame, ExitGame, GameOver }
+
+public enum Scenes { MainMenu = 0, Game = 1 }
